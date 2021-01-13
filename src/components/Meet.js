d554a69b3862c9454d417af931dbf9f6f6839fc6
Kilah Willingham <kilahmay@kilahs-air.lan>
2021-01-12 22:30:04 -0800

    updated resume link and favicon

--- src/components/Meet.js
+++ src/components/Meet.js
@@ -10,7 +10,7 @@ const Meet = () => {
 				alt="kilah's headshot"
 			/>
 			<p className='descriptions'>
-				<a href='https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=b489242c5d&attid=0.1&permmsgid=msg-a:s:2056903379278693115&view=att&disp=inline&saddbat=ANGjdJ_TpfeJVgmtEE7N9xRMrwdDZ3oghGoQNtVzx2VAA45ZjXALtZD-YDL2Ug2n-r62EtizGcCQoHVzmL-r_iETOlepbtvaiw8sOoF8V1g1xvOMAm7ayukZ_0Nj6AIroMnUmIKZzlAfVe5ZQC250KkKFOoPanTM8b6leX2t9BbQ3P4QEmh6gfInRpqiFaRKxXD7icUTF1wzAKyBPrfl6-FJdZ57-vAbN1bpSkZOTwDYesgmVXrmbSfifFLL0ZjPmjigh1M-KeXUhpI4ZZy60C82-FtGMcfmnPtkHBvTPsNZimOkw-RY6a0ZNxzPtEBPyR_BcwyHLcB261Qs2c1iYd76VcL-p4M0CO6BH541EVhZ-CXDwlMIEGP2nN7uqVm135CdXaAL-G7J403-fvt9XTiW3sAulhNa81QuQxx4rmyY3e_JBoe-wPL3uW9HJe9qltmOurwDr-oY8-2YsTy1BdPVU5X_5zP-HzX9SX4d7d7Mc75A7OxMpggd6wg9aHvL72HSFdUCViL-eEhfO-iMsoLXUeAtIlJVlTrLi97iM4PDs8InJPuP2BWWFZl9WGNOZ6E_wqicW9JfWrdrox-He5K819YKY5Gseid0rFlHpmr1H8wShZZBBuThuBe3SIGQ4aZzBISZa9nReU86CGDr_ygkb2x4_CpCcaljBtZS1Q'>
+				<a href='https://drive.google.com/file/d/1ofunQULbgQZRgDhnRulTsjHOkoTphS6k/view?usp=sharing'>
 					<p className='resume'>Resume</p>
 				</a>
 				<br />
